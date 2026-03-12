8e8551641adff987b66128e18732937136e6e561
timfemey <isholaobafemi@gmail.com>
2026-03-12 15:16:01 +0100

    updated test files

--- test/thumbmark_android_flutter_method_channel_test.dart
+++ test/thumbmark_android_flutter_method_channel_test.dart
@@ -5,23 +5,40 @@ import 'package:thumbmark_android_flutter/thumbmark_android_flutter_method_chann
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
 
-  MethodChannelThumbmarkAndroidFlutter platform = MethodChannelThumbmarkAndroidFlutter();
+  MethodChannelThumbmarkAndroidFlutter platform =
+      MethodChannelThumbmarkAndroidFlutter();
   const MethodChannel channel = MethodChannel('thumbmark_android_flutter');
 
   setUp(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(
-      channel,
-      (MethodCall methodCall) async {
-        return '42';
-      },
-    );
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, (MethodCall methodCall) async {
+          switch (methodCall.method) {
+            case 'getPlatformVersion':
+              return '42';
+            case 'generateFingerPrintLow':
+              return 'low_volatility_mock';
+            case 'generateFingerPrintHigh':
+              return 'high_volatility_mock';
+            default:
+              return null;
+          }
+        });
   });
 
   tearDown(() {
-    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger.setMockMethodCallHandler(channel, null);
+    TestDefaultBinaryMessengerBinding.instance.defaultBinaryMessenger
+        .setMockMethodCallHandler(channel, null);
   });
 
   test('getPlatformVersion', () async {
     expect(await platform.getPlatformVersion(), '42');
+  });
+
+  test('generateFingerPrintLow', () async {
+    expect(await platform.generateFingerPrintLow(), 'low_volatility_mock');
+  });
+
+  test('generateFingerPrintHigh', () async {
+    expect(await platform.generateFingerPrintHigh(), 'high_volatility_mock');
   });
 }
--- test/thumbmark_android_flutter_test.dart
+++ test/thumbmark_android_flutter_test.dart
@@ -7,23 +7,47 @@ import 'package:plugin_platform_interface/plugin_platform_interface.dart';
 class MockThumbmarkAndroidFlutterPlatform
     with MockPlatformInterfaceMixin
     implements ThumbmarkAndroidFlutterPlatform {
-
   @override
   Future<String?> getPlatformVersion() => Future.value('42');
+
+  @override
+  Future<String?> getFingerPrintLow() => Future.value('low_mock');
+
+  @override
+  Future<String?> getFingerPrintHigh() => Future.value('high_mock');
 }
 
 void main() {
-  final ThumbmarkAndroidFlutterPlatform initialPlatform = ThumbmarkAndroidFlutterPlatform.instance;
+  final ThumbmarkAndroidFlutterPlatform initialPlatform =
+      ThumbmarkAndroidFlutterPlatform.instance;
 
   test('$MethodChannelThumbmarkAndroidFlutter is the default instance', () {
-    expect(initialPlatform, isInstanceOf<MethodChannelThumbmarkAndroidFlutter>());
+    expect(
+      initialPlatform,
+      isInstanceOf<MethodChannelThumbmarkAndroidFlutter>(),
+    );
   });
 
-  test('getPlatformVersion', () async {
-    ThumbmarkAndroidFlutter thumbmarkAndroidFlutterPlugin = ThumbmarkAndroidFlutter();
-    MockThumbmarkAndroidFlutterPlatform fakePlatform = MockThumbmarkAndroidFlutterPlatform();
-    ThumbmarkAndroidFlutterPlatform.instance = fakePlatform;
+  group('ThumbmarkAndroidFlutter API', () {
+    late ThumbmarkAndroidFlutter thumbmarkPlugin;
+    late MockThumbmarkAndroidFlutterPlatform fakePlatform;
 
-    expect(await thumbmarkAndroidFlutterPlugin.getPlatformVersion(), '42');
+    setUp(() {
+      thumbmarkPlugin = ThumbmarkAndroidFlutter();
+      fakePlatform = MockThumbmarkAndroidFlutterPlatform();
+      ThumbmarkAndroidFlutterPlatform.instance = fakePlatform;
+    });
+
+    test('getPlatformVersion returns correct value', () async {
+      expect(await thumbmarkPlugin.getPlatformVersion(), '42');
+    });
+
+    test('getFingerPrintLow returns correct value', () async {
+      expect(await thumbmarkPlugin.getFingerPrintLow(), 'low_mock');
+    });
+
+    test('getFingerPrintHigh returns correct value', () async {
+      expect(await thumbmarkPlugin.getFingerPrintHigh(), 'high_mock');
+    });
   });
 }
